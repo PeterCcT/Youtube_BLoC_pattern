c17af7a7a59083b448c54513fa1165a03b3c287c
PeterCcT <pedrorodriguesfereira@gmail.com>
2020-05-11 16:41:13 -0300

    Funcionalidade de pesquisa

--- lib/screens/Home.dart
+++ lib/screens/Home.dart
@@ -1,8 +1,37 @@
 import 'package:flutter/material.dart';
+import 'package:youtube_bloc/delegates/data_search.dart';
 
 class HomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      appBar: AppBar(
+        title: Container(
+          height: 50,
+          child: Image.asset('images/yt_logo.png'),
+        ),
+        elevation: 0,
+        backgroundColor: Colors.black,
+        actions: <Widget>[
+          Align(
+            alignment: Alignment.center,
+            child: Text('0'),
+          ),
+          IconButton(
+            icon: Icon(Icons.star),
+            onPressed: () {},
+          ),
+          IconButton(
+            icon: Icon(Icons.search),
+            onPressed: () async {
+              String result =
+                  await showSearch(context: context, delegate: DataSearch());
+                  print(result);
+            },
+          ),
+        ],
+      ),
+      body: Container(),
+    );
   }
 }
